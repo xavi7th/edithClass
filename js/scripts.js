a8e73a42bc053782413763871874a16dcc625e54
leinad <xavi7th@gmail.com>
2018-12-05 12:00:36 +0100

    demo of .load method

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,8 @@
 $(function() {
+	$('#result').load('myfragments/heading')
+
 	/**
-	 * REMEMBER console.log is your best buddy in JS
+	 * NOTE: This will overwrite the result of the first line
 	 */
-
-	$('.calculator__input-buttons').click(function(e) {
-		/**
-		 * This prevents the default action that should have been carried out.
-		 * In this case submitting the form
-		 */
-		e.preventDefault()
-
-		/**
-		 * This retrieves the text content of the currently clicked button
-		 * NOTE: $(this)
-		 */
-		buttonValue = $(this).text()
-
-		/**
-		 * This retrieves the current value of the input field
-		 * NOTE .val() for input fields instead of .text()
-		 */
-		oldContent = $('.calculator__display').val()
-
-		/**
-		 * Check if the button that was clicked is not a number (NaN)
-		 */
-		if (isNaN(buttonValue)) {
-			/**
-			 * Check if they have previously saved an operation
-			 */
-			if ($('.calculator__operation').val().length != 0) {
-				/**
-				 * In case you want to trigger a form submit instead of an alert
-				 */
-				// $('form').submit()
-				alert(
-					'You have chosen an operation already. Just hit the damned enter button already'
-				)
-				return
-			}
-
-			if (buttonValue == '+') {
-				operation = 'addition'
-			} else if (buttonValue == '-') {
-				operation = 'subtraction'
-			} else if (buttonValue == 'x') {
-				operation = 'times'
-			} else if (buttonValue == '/') {
-				operation = 'divide'
-			} else if (buttonValue == '%') {
-				operation = 'remainder'
-			}
-
-			/**
-			 * Saving the operation in the operation input field
-			 * Saving the current value of the display in the first value input field
-			 * Clear the display input field so that they start entering the other number
-			 */
-			$('.calculator__operation').val(operation)
-			$('.calculator__first-value').val(oldContent)
-			$('.calculator__display').val(0)
-
-			return
-		}
-
-		if (oldContent == 0) {
-			newContent = buttonValue
-		} else {
-			newContent = oldContent + buttonValue
-		}
-
-		/**
-		 * This sets the input field's value to a new value
-		 * NOTE: There is a parameter inside .val()
-		 */
-		$('.calculator__display').val(newContent)
-	})
+	$('#result').load('myfragments/process.php')
 })
