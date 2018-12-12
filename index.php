db6dbda288d955811d31da3b115253355fe51881
leinad <xavi7th@gmail.com>
2018-12-12 12:03:21 +0100

    Basic PhpMysql linking

--- index.php
+++ index.php
@@ -1,13 +1,59 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Document</title>
-</head>
-<body>
-		<a href="allusers.php">View All Users</a>
-		<a href="allusers.php?new=jane">Add User Efua</a>
-</body>
-</html>
+<?php
+
+$con = mysqli_connect('localhost', 'root', '', 'edit');
+
+if (!$con) {
+	/**
+	 * !The connection request failed
+	 */
+
+	echo mysqli_connect_error();
+
+	exit;
+}
+
+$editSql = "INSERT INTO efua (id, name, time, initials) VALUES(2, 'Dere', '2018-11-11 12:00:01', 'et')";
+
+/**
+ * Send the query to mysql
+ * @param mysqli Resource $con
+ * @param string $editSql
+ */
+$query = mysqli_query($con, $editSql);
+if (!$query) {
+	/**
+	 * !The query request failed
+	 */
+	echo mysqli_error($con);
+	exit;
+}
+
+
+/**
+ * ? Testing select queries
+ */
+$editSql = "SELECT * FROM efua";
+
+/**
+ * Send the query to mysql
+ * @param mysqli Resource $con
+ * @param string $editSql
+ */
+$query = mysqli_query($con, $editSql);
+if (!$query) {
+	/**
+	 * !The query request failed
+	 */
+	echo mysqli_error($con);
+	exit;
+}
+
+/**
+ * ? We retrieve our results in a loop in case there are more than one
+ * * INFO: mysqli_fetch_assoc($query) gives one result at a time
+ */
+while ($result = mysqli_fetch_assoc($query)) {
+	$results[] = $result;
+}
+
+print_r($results);
